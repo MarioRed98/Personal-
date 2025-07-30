af1e7927c96fdf2bec8c4e6e5d687e4ade28e4da
Mario98 <mariokaka98@live.it>
2025-07-30 20:27:29 +0200

    Delete Profilo Personale directory

--- Profilo Personale/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const toggle = document.getElementById("menu-toggle");
-    const navLinks = document.getElementById("nav-links");
-  
-    toggle.addEventListener("click", function () {
-      navLinks.classList.toggle("show");
-    });
-  });
-  
